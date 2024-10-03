2cda17e9bd4642918413935257b5d45941157127
catalogo12 <96148687+catalogo12@users.noreply.github.com>
2024-10-03 17:57:52 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //trabajoFinal-index.js
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/baseDatosJose1');
+mongoose.connect('mongodb://mongo:pckblSmGkVEJYHxXXFXqlwVqgZxMZIVG@mongodb.railway.internal:27017/baseDatosJose1');
 const express = require("express");
 const app=express();
 const cors = require("cors");
